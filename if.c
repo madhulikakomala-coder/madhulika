88669daf015aaf06e8f390e676ff30dd06e2596c
madhulikakomala-coder <madhulikakomala@gmail.com>
2025-11-23 17:03:55 +0530

    Add files via upload

---
+++ if.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("enter a number");
+	scanf("%d",&a);
+	if(a>0);
+	printf("given number is positive");
+}
---
+++ ifelse-2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a=4;
+	int b=6;
+	if(a>b){
+		printf("a is greater");
+	}
+	else{
+		printf("b is greater");
+	}
+	return 0;
+}
